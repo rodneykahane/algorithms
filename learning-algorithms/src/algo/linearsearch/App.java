3a3d56addcb566a86da0ebfe91ada719c93044d2
rodney <rodney@rodneykahan.es>
2018-07-04 13:51:29 -0400

    linearSearch complete

--- learning-algorithms/src/algo/linearsearch/App.java
+++ learning-algorithms/src/algo/linearsearch/App.java
@@ -3,9 +3,14 @@ package algo.linearsearch;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] dataSet = {1,3,2,4,5,9,7,22,};
+		int x = 25;		
+		
+		LinearSearch search = new LinearSearch();		
+		
+		System.out.println("the index is "+search.searchArray(dataSet,x));		
 
 	}
 
 }
-//
--- learning-algorithms/src/algo/linearsearch/LinearSearch.java
+++ learning-algorithms/src/algo/linearsearch/LinearSearch.java
@@ -2,5 +2,15 @@ package algo.linearsearch;
 
 public class LinearSearch {
 
-}
-//
+	public int searchArray(int[] a, int x) {		
+		int answer=-1;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==x) {
+				answer = i;
+				return answer;
+			}
+		}
+		return answer;
+	}
+
+}//end class
