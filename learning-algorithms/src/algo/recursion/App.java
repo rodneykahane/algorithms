f865a1acace0e39bf4749d6e53efbd7e851f402e
rodney <rodney@rodneykahan.es>
2018-07-11 19:16:09 +0000

    selection sort

--- learning-algorithms/src/algo/recursion/App.java
+++ learning-algorithms/src/algo/recursion/App.java
@@ -6,20 +6,68 @@ public class App {
 	public static void main(String[] args) {
 		
 		//int[] a = {5,4,2,8,3,9,1,7};
-		int[] a = {1,2,3,4,5,6,7,8,9};
+		//int[] a = {1,2,3,4,5,6,7,8,9};
+		int [] a = {25,47,3,19,8,18};
 		//RecursiveLinearSearch search = new RecursiveLinearSearch();
-		RecursiveBinarySearch search = new RecursiveBinarySearch();
+		//RecursiveBinarySearch search = new RecursiveBinarySearch();
 		
 		//reduceByOne(10);
 		//System.out.println(search.rLinearSearch(a,0,1));
-		System.out.println(search.rBinarySearch(a, 0, a.length-1,9 ));
-	}
+		//System.out.println(search.rBinarySearch(a, 0, a.length-1,9 ));
+		
+		//sort(a);
+		selectSort(a);
+	}//end main
 	
 	public static void reduceByOne (int n) {
 		if(n>=0) {
 			reduceByOne(n-1);
 		}
 		System.out.println("completed with call "+n);
-	}
+	}//end reduceByOne
 
-}
+	public static void sort(int[] a) {
+		int tmp;		
+		
+		for(int j=0;j<a.length;j++) {
+			
+			for(int i=1;i<a.length;i++) {
+				if(a[i]<a[i-1]) {					
+					tmp=a[i];
+					a[i] = a[i-1];
+					a[i-1]=tmp;
+				}			
+			}			
+		}		
+	}//end sort
+	
+	public static void selectSort(int[] a) {
+		int smallest=99;
+		int tmp=-1;
+		int index=0;
+		
+		for(int i=0;i<a.length;i++) {
+			
+			if(a[i]<smallest) {
+				smallest = a[i];
+				System.out.println("smallest is: "+smallest);
+				index = i;	
+				System.out.println("index is: "+index);
+				tmp = a[0];
+			}
+			
+			a[0]=smallest;
+			a[index]=tmp;
+			
+		}
+		
+		for (int i=0;i<a.length;i++) {
+			System.out.print(a[i]+", ");
+		}
+		
+	}
+	
+	
+	
+	
+}//end class
---
+++ learning-algorithms/src/algo/selectionsort/App.java
@@ -0,0 +1,28 @@
+package algo.selectionsort;
+
+public class App {
+
+	public static void main(String[] args) {
+		int[] myArray = selectionSort(new int[] {9,8,3,13,87,12,99});
+		
+		for(int i=0;i<myArray.length;i++) {
+			System.out.print(myArray[i]);
+		}
+
+	}//end main
+	
+//for i=0 to A.length-1
+	//set minimum to i
+	//for j=i+1 to A.length
+	   //if A[j] < A[minimum]
+			//set minimum to j
+	//swap A[i] with A[minimum]
+	public static int[] selectionSort(int[] a) {
+		int[] sortedArray;
+		
+		
+		
+		return sortedArray;
+	}//end selectionSort
+
+}//end class
