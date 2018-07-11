afa566963f3d444b98c343f55912a9735f567ae5
rodney <rodney@rodneykahan.es>
2018-07-11 20:50:41 +0000

    heading home

--- learning-algorithms/src/algo/selectionsort/App.java
+++ learning-algorithms/src/algo/selectionsort/App.java
@@ -1,12 +1,14 @@
 package algo.selectionsort;
 
+import java.util.Arrays;
+
 public class App {
 
 	public static void main(String[] args) {
 		int[] myArray = selectionSort(new int[] {9,8,3,13,87,12,99});
 		
 		for(int i=0;i<myArray.length;i++) {
-			System.out.print(myArray[i]);
+			System.out.print(myArray[i]+", ");
 		}
 
 	}//end main
@@ -17,18 +19,26 @@ public class App {
 	   //if A[j] < A[minimum]
 			//set minimum to j
 	//swap A[i] with A[minimum]
-	public static int[] selectionSort(int[] a) {
-		int[] sortedArray;
-		int minimum;
+	public static int[] selectionSort(int[] a) {				
 		
-		for(int i=0;i<a.length;i++) {
-			minimum=a[i];
+		for(int i=0;i<a.length-1;i++) {
+			int minimum = i;
 			
 			for(int j=0;j<a.length;j++) {
-				if (a[j] < minimum) {
-					minimum = a[j];
+				if (a[j] < a[minimum]) {
+					minimum = j;
+					//System.out.println("minimum is: "+ minimum);	
+					
+					
 				}
+				
+				
 			}
+			int tmp = a[i];				
+			a[i] = a[minimum];
+			a[minimum] = tmp;
+			System.out.println(Arrays.toString(a));
+			
 		}
 		
 		
