baf648a107409a964ad436e16230f5f42bc44833
rodney <rodney@rodneykahan.es>
2018-07-12 20:31:03 +0000

    end of the day

---
+++ learning-algorithms/src/algo/mergesort/App.java
@@ -0,0 +1,12 @@
+package algo.mergesort;
+
+import java.util.Arrays;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		//System.out.println(Arrays.toString(insertionSort(a)));
+	}//end main
+
+}//end class
