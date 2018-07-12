d2c98c2f16784bb42b13c667c9b48f88dc4c06f9
rodney <rodney@rodneykahan.es>
2018-07-12 15:58:04 +0000

    working insertion sort

--- learning-algorithms/src/algo/insertionsort/App.java
+++ learning-algorithms/src/algo/insertionsort/App.java
@@ -1,9 +1,13 @@
 package algo.insertionsort;
 
+import java.util.Arrays;
+
 public class App {
 
 	public static void main(String[] args) {
+		int[] a = {25,47,3,19,8,18};
 		
+		System.out.println(Arrays.toString(insertionSort(a)));
 
 	}//end main
 
@@ -17,7 +21,15 @@ public class App {
 	
 	
 	public static int[] insertionSort(int[] a) {
-		
+		for(int i=1;i<a.length;i++) {
+			int element = a[i];
+			int j = i-1;
+			while(j>=0 && a[j]> element) {
+				a[j+1]=a[j];
+				j--;
+			}
+			a[j+1] = element;
+		}
 		
 		return a;
 	}
