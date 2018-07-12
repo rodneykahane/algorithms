0698647e01164f0ff39e50f9e380f2f86aa4cb8c
rodney <rodney@rodneykahan.es>
2018-07-12 16:08:46 +0000

    code clean up

--- learning-algorithms/src/algo/insertionsort/App.java
+++ learning-algorithms/src/algo/insertionsort/App.java
@@ -9,16 +9,7 @@ public class App {
 		
 		System.out.println(Arrays.toString(insertionSort(a)));
 
-	}//end main
-
-	
-	//for i=1 to A.length-1
-	//	set element to A[i], and set j to i-1
-	//		while j>=0 and A[j] > element
-	//			set A[j+1] to A[j]
-	//			decrement j by 1
-	//		set A[j+1] to element
-	
+	}//end main	
 	
 	public static int[] insertionSort(int[] a) {
 		for(int i=1;i<a.length;i++) {
