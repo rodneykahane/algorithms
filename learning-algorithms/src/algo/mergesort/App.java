111798677e42bb01cc53a19fabd729ed3974a2e9
rodney <rodney@rodneykahan.es>
2018-07-15 12:02:51 -0400

    my solution for merge

--- learning-algorithms/src/algo/mergesort/App.java
+++ learning-algorithms/src/algo/mergesort/App.java
@@ -6,11 +6,13 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		int[] inputArray = {9,7,3,1,6,3,2,6,8,9,2,3,0};
+		//int[] inputArray = {9,7,3,1,6,3,2,6,8,9,2,3,0};
+		
+		int[] inputArray = {12,1,11,2,10,3,9,4,8,5,7,6};
 		MergeSort sorter = new MergeSort();
 		
-		sorter.sort(inputArray);
-		//System.out.println(Arrays.toString(insertionSort(a)));
+		//System.out.println(sorter.sort(inputArray));
+		System.out.println(Arrays.toString(sorter.sort(inputArray)));
 
 		
 	}//end main
--- learning-algorithms/src/algo/mergesort/MergeSort.java
+++ learning-algorithms/src/algo/mergesort/MergeSort.java
@@ -4,25 +4,42 @@ import java.util.Arrays;
 
 public class MergeSort {
 	
-	public static void sort (int a[]) {
-			sort(a,0, a.length-1);
+	public static int[] sort (int a[]) {
+			int[] b = sort(a,0, a.length-1);
+			
+			return b;
 		
 	}//end sort	
 	
-	public static void sort(int[] a, int start, int end) {
+	public static int[] sort(int[] a, int start, int end) {
 				
 				if(end<=start) {
-					return;
+					int[] x = {-1};
+					return  x;
 				}
 				
 				int mid = (start+end)/2;				
 				sort(a,start,mid);				
 				sort(a,mid+1,end);
-				merge(a,start,mid,end);		
+				int[] b = merge(a,start,mid,end);
+				
+				return b;
 				
 		}//end sort
 	
-	public static void merge(int a[], int start, int mid, int end) {
+	public static int[] merge(int a[], int start, int mid, int end) {
+		
+		for(int i=1;i<a.length;i++) {
+			
+			if(a[i-1]>a[i]) {
+				int tmp;
+				tmp = a[i];
+				a[i]=a[i-1];
+				a[i-1]=tmp;				
+			}
+		}		
+		//System.out.println(Arrays.toString(a));
+		return a;
 		
 	}//end merge
 
